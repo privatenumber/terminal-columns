2347324fab362c1987d278ec3da40de3bdb4cc1a
Hiroki Osame <hiroki.osame@gmail.com>
2022-01-01 20:46:30 -0500

    style: upgrade eslint library

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,7 @@ import { breakpoints } from './breakpoints';
 import type { Options } from './types';
 
 export {
-	// eslint-disable-next-line unicorn/prefer-export-from
 	terminalColumns as default,
-
-	// eslint-disable-next-line unicorn/prefer-export-from
 	breakpoints,
-
-	// eslint-disable-next-line unicorn/prefer-export-from
 	Options,
 };
